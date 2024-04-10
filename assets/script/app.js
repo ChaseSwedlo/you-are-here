d4a6c8e3ba5af436432e99816d257282d40966b6
Chase Swedlo <chasechaseswedlo@outlook.com>
2024-04-10 18:18:53 -0500

    Change search bar style aswell as a few other styles

--- assets/script/app.js
+++ assets/script/app.js
@@ -2,15 +2,18 @@
 
 const mapBox = document.getElementById('map');
 const find = document.querySelector('.find-you');
-let userLatitude = 40;
-let userLongitude = -74.5;
+const searchBar = document.querySelector('.search-bar');
+const searchButton = document.querySelector('.search');
+let userLatitude = 40.758896;
+let userLongitude = -73.985130;
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiY2hhc2Vzd2VkbG8iLCJhIjoiY2x1cmlwenlmMDFzNjJxbW9jcjF4eHNreSJ9.vHG-jnm9uU-wInuQjToVQw';
 const map = new mapboxgl.Map({
 	container: 'map',
 	style: 'mapbox://styles/mapbox/streets-v12',
 	center: [userLongitude, userLatitude],
-	zoom: 7
+    zoom: 16,
+    pitch: 55
 });
 
 function getLocation(position) {
@@ -30,15 +33,16 @@ const options = {
 
 function buildMap() {
     map.jumpTo({
-        center: [userLongitude, userLatitude],
-        zoom: 14.5,
-        pitch: 55
+        center: [userLongitude, userLatitude]
     });
-    map.setPitch(20, {duration: 2000});
     new mapboxgl.Marker()
     .setLngLat([userLongitude, userLatitude])
     .addTo(map);
 }
+
+searchButton.addEventListener("click", () => {
+    searchBar.value = '';
+});
 
 find.addEventListener("click", () => {
     if('geolocation' in navigator) {
